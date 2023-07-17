d45bdaf8f01a517e9adc9c209f076393ef162c10
wSilber <silberstein75@gmail.com>
2023-07-17 14:26:45 -0400

    Added session to app

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,11 @@
 // import '@/styles/globals.css'
+import { SessionProvider } from 'next-auth/react'
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
 
     return (
-        <Component {...pageProps} />
+        <SessionProvider {...pageProps}>
+            <Component {...pageProps} />
+        </SessionProvider>
     )
 }
