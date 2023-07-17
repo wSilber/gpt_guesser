d47d9a96f41008bd1fe24ce9d1170278eb946315
wSilber <silberstein75@gmail.com>
2023-07-17 14:54:55 -0400

    Got rid of unused providers

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -10,24 +10,10 @@ const PROVIDERS = {
 
 export default NextAuth({
   providers: [
-    // OAuth authentication providers...
-    // AppleProvider({
-    //   clientId: process.env.APPLE_ID,
-    //   clientSecret: process.env.APPLE_SECRET
-    // }),
-    // FacebookProvider({
-    //   clientId: process.env.FACEBOOK_ID,
-    //   clientSecret: process.env.FACEBOOK_SECRET
-    // }),
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
-    // // Passwordless / email sign in
-    // EmailProvider({
-    //   server: process.env.MAIL_SERVER,
-    //   from: 'NextAuth.js <no-reply@example.com>'
-    // }),
   ],
   callbacks: {
     async signIn({ account, profile} ) {
