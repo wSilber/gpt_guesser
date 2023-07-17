10a5e8bd4f78cd22d75a999be7ab3d46e851e904
wSilber <silberstein75@gmail.com>
2023-07-17 14:16:59 -0400

    Completed Initial Setup

---
+++ pages/_app.jsx
@@ -0,0 +1,8 @@
+// import '@/styles/globals.css'
+
+export default function App({ Component, pageProps: { session, ...pageProps } }) {
+
+    return (
+        <Component {...pageProps} />
+    )
+}
---
+++ pages/api/test.js
@@ -0,0 +1,3 @@
+export default async function test(req, res) {
+    res.send({test: 'test'})
+}
