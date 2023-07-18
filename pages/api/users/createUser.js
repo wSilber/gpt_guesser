de6fb4e15e449c98ec764886bfc2dab1f2358fec
wSilber <silberstein75@gmail.com>
2023-07-18 19:58:39 -0400

    Added signup and signin capabilities

---
+++ pages/api/users/createUser.js
@@ -0,0 +1,33 @@
+import { prisma } from "@/prisma/prisma";
+ 
+export default async function checkUserExists(request, response) {
+
+    if(request.method !== 'PUT') {
+        console.error(`ERROR: ${request.method} attempted on endpoint api/users/checkUserExists`)
+        response.status(405).json({error: 'This endpoint only accepts PUT requests'})
+        return
+    }
+
+    const account = request.body.userAccount
+
+    if(!account) {
+        console.error(`ERROR: No account provided`)
+        response.status(400).json({error: 'User not provided'})
+    }
+
+  const prismaUser = await prisma.user.upsert({
+    where: {email: account.email},
+    update: {},
+    create: {
+      email: account.email,
+      username: account.email,
+      image: account.image,
+      googleId: account.googleId
+    }
+  })
+
+  console.log(prismaUser)
+ 
+  response.status(200).json({ user: prismaUserser });
+  
+}
