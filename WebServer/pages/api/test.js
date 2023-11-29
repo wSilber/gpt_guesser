2726ee25ac4ba2b2610671156f92efeb9b2f2902
wSilber <silberstein75@gmail.com>
2023-11-29 18:16:23 -0500

    Added network connection between webserver and redis

--- WebServer/pages/api/test.js
+++ WebServer/pages/api/test.js
@@ -1,13 +1,28 @@
+import { createClient } from 'redis'
 
 export default async function handler(request, response) {
 
   console.log("Starting test")
 
-  // const user = await prisma.user.findFirst({
-  //   where: {
-  //     email: 'test@test.com'
-  //   }
-  // })
- 
-  return response.status(200).json({ user: "test"});
+  try {
+
+    const client = createClient({url: "redis://redis:6379"})
+
+    await client.connect()
+
+    let value = await client.get('key');
+
+    await client.set('key', 'value');
+    value = await client.get('key');
+
+    await client.disconnect()
+
+    return response.status(200).json({test: value});
+
+  } catch (err) {
+
+    console.log("ERROR found")
+    console.log(err)
+    return response.status(200).json({error: err});
+  }
 }
